0c3febde394946f74d2d513b197b7ddc244cca60
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-17 12:59:06 +1300

    now using dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 80,
+    port: process.env.CONNECT_PORT,
   },
 });
