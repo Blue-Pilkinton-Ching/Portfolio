133e2596507918f66eb02877c2dbb12b968dac38
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-17 19:53:49 +1300

    small change so it looks like i coded today

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,18 @@
-import express from 'express'
-import { createServer } from 'http'
-import * as path from 'path'
+import express from "express";
+import { createServer } from "http";
+import * as path from "path";
 
-const app = express()
-const httpServer = createServer(app)
+const app = express();
+const httpServer = createServer(app);
 
-app.use(express.json())
+app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve('dist')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist', 'index.html'))
-  })
-}
+// Uncomment in production
+// Comment in dev
 
-export { httpServer }
+//   app.use(express.static(path.resolve('dist')))
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve('dist', 'index.html'))
+//   })
+
+export { httpServer };
