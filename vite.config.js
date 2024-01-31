308e00e94a9bcc6892d6371e8c87894936da77bc
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-01-31 18:56:24 +1300

    Fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import "dotenv/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: process.env.CONNECT_PORT,
+    port: 3000,
   },
 });
