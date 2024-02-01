a384a162585b436013dce928c0bd84f13e429414
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2024-02-01 21:20:37 +1300

    changed agaggagians

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import { httpServer } from "./server"; // Adjust the import to a named import
 
-const port = process.env.SERVER_PORT || 80;
+const port = process.env.PORT || 3000;
 
 httpServer.listen(port, function () {
   console.log("Listening on port", port); // Removed eslint-disable-line for brevity
